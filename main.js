092709da979bcb99be0e0c1eaa20066ebfd682f3
harshpratap3 <harshpratapshikarwar@gmail.com>
2025-07-03 23:01:24 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ["Web Designer", "Frontend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true
+  });
+  
